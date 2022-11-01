94ed7c9033c18a5de38d8c585f30cf0808784462
Pranav Addepalli <pranav.addepalli@gmail.com>
2022-10-31 21:38:29 -0400

    created documentation for some parameters of /predict using swagger

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from app.handlers.routes import configure_routes
+from flasgger import Swagger, LazyString, LazyJSONEncoder
+from flasgger import swag_from
+
 
 app = Flask(__name__)
-
 configure_routes(app)
+swagger = Swagger(app)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=80)
--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -4,6 +4,8 @@ import joblib
 import pandas as pd
 import numpy as np
 import os
+from flasgger import Swagger, LazyString, LazyJSONEncoder
+from flasgger import swag_from
 
 def configure_routes(app):
 
@@ -13,11 +15,86 @@ def configure_routes(app):
 
     @app.route('/')
     def hello():
+        """Home page for the application
+    ---
+    responses:
+      200:
+        description: Successful response
+    """
         return "try the predict route it is great!"
-
 
     @app.route('/predict')
     def predict():
+        """Route to predict a student's success based on some factors
+    ---
+    parameters:
+      - in: query
+        name: absences
+        type: int
+        description: 'Number of school absences (numeric: from 0 to 93)'
+        required: true
+        default: '0'
+      - in: query
+        name: health
+        type: int
+        description: 'Current health status (numeric: from 1 - very bad to 5 - very good)'
+        enum: ['1', '2', '3', '4', '5']
+        required: true
+        default: '1'
+      - in: query
+        name: internet
+        type: int
+        description: 'Internet access at home (binary: yes or no)'
+        enum: ['0', '1']
+        required: true
+        default: '0'
+      - in: query
+        name: higher
+        type: int
+        description: 'Wants to take higher education (binary: yes or no)'
+        enum: ['0', '1']
+        required: true
+        default: '0'
+      - in: query
+        name: paid
+        type: int
+        description: 'Extra paid classes within the course subject (Math or Portuguese) (binary: yes or no)'
+        enum: ['0', '1']
+        required: true
+        default: '0'
+      - in: query
+        name: paid
+        type: int
+        description: 'Extra paid classes within the course subject (Math or Portuguese) (binary: yes or no)'
+        enum: ['0', '1']
+        required: true
+        default: '0'
+      - in: query
+        name: famsup
+        type: int
+        description: 'Family educational support (binary: yes or no)'
+        enum: ['0', '1']
+        required: true
+        default: '0'
+      - in: query
+        name: schoolsup
+        type: int
+        description: 'Extra educational support (binary: yes or no)'
+        enum: ['0', '1']
+        required: true
+        default: '0'
+        
+    responses:
+      200:
+        description: Successful response
+      400:
+        description: Missing parameters
+      406:
+        description: Invalid type
+      422:
+        description: Out of range
+    """
+
         #use entries from the query string here but could also use json
         age = request.args.get('age')
         absences = request.args.get('absences')
@@ -30,4 +107,4 @@ def configure_routes(app):
         })
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
-        return jsonify(np.asscalar(prediction))
+        return jsonify(np.ndarray.item(prediction))
